eece6d4ae7d263e14c65da702cd6041e49d816d2
mmmmmmmmn <mygithubemailllll@gmail.com>
2020-04-11 21:23:44 +0900

    implement FLIP

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,56 +7,79 @@ import { useSprings, animated } from 'react-spring'
 import { useDrag } from 'react-use-gesture'
 
 const Index: NextComponentType = () => {
-    const data = [1, 2]
+    const [data, setData] = React.useState(['red', 'blue', 'green', 'purple', 'yellow'])
 
-    const [springs, set] = useSprings(data.length, () => ({
-        y: 0,
-        scale: 1,
-        zIndex: 0,
-        immediate: true,
-    }))
+    const [springs, set] = useSprings(data.length, () => initialSpringValues)
 
-    const bind = useDrag(
-        state => {
-            const {
-                args: [originalIndex],
-                movement: [, my],
-                down,
-            } = state
+    const bind = useDrag(({ args: [originalIndex], movement: [, my], down }) => {
+        const goal = clamp(originalIndex + getDistance(my), 0, data.length - 1)
 
-            set(index =>
-                down && originalIndex === index
-                    ? {
-                          y: my,
-                          scale: 1.5,
-                          zIndex: 1000,
-                          immediate: true,
-                      }
-                    : {
-                          y: 0,
-                          scale: 1,
-                          zIndex: 0,
-                          immediate: true,
-                      },
-            )
-        },
-        {
-            axis: 'y',
-        },
-    )
+        const min = Math.min(originalIndex, goal)
+        const max = Math.max(originalIndex, goal)
+        const dir = Math.sign(my)
+
+        if (!down) {
+            if (originalIndex !== goal) {
+                // 並べ替え
+                setData(data =>
+                    [...Array(data.length).keys()]
+                        .map(rawIndex => {
+                            if (rawIndex === goal) return originalIndex
+                            if (min <= rawIndex && rawIndex <= max) return rawIndex + dir
+
+                            return rawIndex
+                        })
+                        .map(sortedIndex => data[sortedIndex]),
+                )
+                set(index => {
+                    if (goal === index)
+                        return {
+                            y: my - (height + between) * (goal - originalIndex),
+                            z: 10,
+                            scale: 1.5,
+                            immediate: true,
+                        }
+
+                    return { ...initialSpringValues, immediate: true }
+                })
+            }
+
+            set(initialSpringValues)
+            return
+        }
+
+        set(index => {
+            if (originalIndex === index)
+                return {
+                    y: my,
+                    z: 10,
+                    scale: 1.5,
+                    immediate: key => ['y', 'z'].includes(key),
+                }
+
+            return {
+                y: min <= index && index <= max ? -1 * dir * (height + between) : 0,
+                z: 0,
+                immediate: key => ['z'].includes(key),
+            }
+        })
+    })
 
     return (
         <>
-            {springs.map(({ y, scale, zIndex }, i) => (
+            {springs.map(({ y, z, scale }, i) => (
                 <Target
-                    key={i}
+                    key={data[i]}
                     {...bind(i)}
                     style={{
-                        zIndex,
-                        top: y,
-                        transform: scale.to(s => `scale(${s})`),
+                        y,
+                        zIndex: z,
+                        scale,
                     }}
-                />
+                    bg={data[i]}
+                >
+                    {data[i]}
+                </Target>
             ))}
         </>
     )
@@ -64,17 +87,43 @@ const Index: NextComponentType = () => {
 
 export default Index
 
-const Target = styled(animated.div)`
-    height: 100px;
-    width: 100px;
-    position: relative;
-    background: red;
+const height = 100
+const between = 10
+
+const initialSpringValues = {
+    y: 0,
+    z: 1,
+    scale: 1,
+    immediate: false,
+}
+
+const getDistance = (y: number): number => {
+    const absDistance = (positiveY => {
+        if (0 <= positiveY && positiveY < height * 0.5) return 0
+
+        return Math.floor((positiveY - height * 0.5) / (height + between))
+    })(Math.abs(y))
+
+    return Math.sign(y) * absDistance
+}
+
+const clamp = (target: number, min: number, max: number) => Math.min(Math.max(target, min), max)
+
+const Target = styled(animated.div)<{ bg: string }>`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
     border: 1px solid;
     cursor: pointer;
+    box-sizing: border-box;
+
+    height: ${height}px;
     user-select: none;
+    position: relative;
+    background: ${p => p.bg};
 
     & + & {
-        margin-top: 10px;
-        background: blue;
+        margin-top: ${between}px;
     }
 `
