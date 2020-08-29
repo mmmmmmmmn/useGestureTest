12b5867ca0d0a6c392d62383cfa9666b63cd20dd
mmmmmmmmn <mygithubemailllll@gmail.com>
2020-08-29 20:35:27 +0900

    update lint config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-    '*.{js,ts,tsx,md,json,css}': ['prettier --write', 'git add'],
-    // '*.{js,ts,tsx}': ['eslint'], // eslintをかけてerrorのものがあればコミットしない設定を後で足したい
+    '*.{js,ts,tsx}': ['eslint --fix --ignore-path .gitignore', 'git add'],
 }
