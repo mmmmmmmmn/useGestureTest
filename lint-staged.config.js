67447e7f0354ab3b17943454fe04730f0a2f4427
mmmmmmmmn <mygithubemailllll@gmail.com>
2020-09-20 23:48:00 +0900

    update lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    '*.{md,json,scss,css}': ['prettier --write --ignore-path .gitignore', 'git add'],
-    '*.{js,ts,tsx}': ['eslint --fix --ignore-path .gitignore', 'git add'],
+    '*.{md,json,scss,css}': ['prettier --write', 'git add'],
+    '*.{js,ts,tsx}': ['eslint --fix', 'git add'],
 }
